c9222b8a455611b8ab1ea1919d67bc91210c6bf2
Ioane5 <ioane5@Ioanes-PC.local>
2015-12-19 23:25:52 +0400

    gallery implemented

--- app/src/main/java/ge/steps/selfie_lapse/MainActivity.java
+++ app/src/main/java/ge/steps/selfie_lapse/MainActivity.java
@@ -1,13 +1,111 @@
 package ge.steps.selfie_lapse;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageGridAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // adapter & gallery stuff
+        mAdapter = new ImageGridAdapter();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerView.setAdapter(mAdapter);
+        FileStorage storage = FileStorage.getSelfieStore(getApplicationContext());
+        mAdapter.setData(storage.getAllSelfies());
+    }
+
+    class ImageGridAdapter extends RecyclerView.Adapter<ImageGridAdapter.ImageVH> {
+
+        private List<Selfie> mData;
+
+        class ImageVH extends RecyclerView.ViewHolder {
+            ImageView image;
+
+            public ImageVH(View itemView) {
+                super(itemView);
+                image = (ImageView) itemView.findViewById(R.id.image);
+            }
+        }
+
+        public void setData(List<Selfie> data) {
+            mData = data;
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public ImageVH onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ImageVH(LayoutInflater.from(getApplicationContext()).inflate(R.layout.selfie_view, null, false));
+        }
+
+        @Override
+        public void onBindViewHolder(final ImageVH holder, int position) {
+            final Selfie selfie = mData.get(position);
+            holder.image.setTag(selfie.getPath());
+            holder.image.setImageResource(R.drawable.ic_image_placeholder);
+            new AsyncTask<Void, Void, Bitmap>() {
+                @Override
+                protected Bitmap doInBackground(Void... params) {
+                    File image = new File(selfie.getPath());
+                    BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+                    bmOptions.inSampleSize = 4;
+                    return BitmapFactory.decodeFile(image.getAbsolutePath(), bmOptions);
+                }
+
+                @Override
+                protected void onPostExecute(final Bitmap bitmap) {
+                    super.onPostExecute(bitmap);
+                    if (TextUtils.equals((String) holder.image.getTag(), selfie.getPath())) {
+                        holder.image.animate().alpha(0).withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                holder.image.setImageBitmap(bitmap);
+                                holder.image.animate().alpha(1);
+                            }
+                        });
+                    }
+                }
+            }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mData == null ? 0 : mData.size();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/ge/steps/selfie_lapse/SquareLayout.java
@@ -0,0 +1,30 @@
+package ge.steps.selfie_lapse;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * A RelativeLayout that will always be square -- same width and height,
+ * where the height is based off the width.
+ */
+public class SquareLayout extends RelativeLayout {
+
+    public SquareLayout(Context context) {
+        super(context);
+    }
+
+    public SquareLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // Set a square layout.
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+
+}
